930cee2733cf35af5935648db7bab7bb0e738b63
Frederick Robinson <frederick.robinson@gmail.com>
2016-08-14 00:55:11 -0700

    script improvements. found some cases.

--- w22/6_test/generate.sh
+++ w22/6_test/generate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-for x in `seq 9`; do python input.py > $x.in; done
-for x in `seq 9`; do python output.py < $x.in > $x.out; done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SIZE=100
+
+for x in `seq $SIZE`; do python $DIR/input.py > $DIR/$x.in; done
+for x in `seq $SIZE`; do python $DIR/output.py < $DIR/$x.in > $DIR/$x.out; done
--- w22/test.sh
+++ w22/test.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
 NAME=6
-for input in ${NAME}_test/*.in; do python ${NAME}.py < $input > /tmp/output; diff /tmp/output `echo $input | sed 's/in$/out/'` ; done
+for input in ${NAME}_test/*.in
+do
+	python ${NAME}.py < $input > /tmp/output
+	output=`echo $input | sed 's/in$/out/'`
+	cmp -s "/tmp/output" "$output" 
+	if [[ $? -ne 0 ]]
+	then
+		echo "$input"
+		diff "/tmp/output" "$output"
+	fi
+done
